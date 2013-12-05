32f463b1544abdd9b1bf0a741230f15ef0c40a10
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-12-04 23:47:37 -0500

    BlogPost Comments as a nested resource

--- javascripts/app/ember-blog-app.js
+++ javascripts/app/ember-blog-app.js
@@ -19,6 +19,13 @@ EmberBlog.ApplicationAdapter = DS.FixtureAdapter.extend();
 // A simple client-side model for a blog post
 EmberBlog.BlogPost = DS.Model.extend({
   title: DS.attr('string'),
+  body: DS.attr('string'),
+  comments: DS.hasMany('BlogPostComment', {async:true})
+});
+
+// A BlogPost HAS MANY BlogPostComments and a BlogPostComment BELONGS TO a BlogPost
+EmberBlog.BlogPostComment = DS.Model.extend({
+  post: DS.belongsTo('BlogPost'),
   body: DS.attr('string')
 });
 
@@ -27,8 +34,11 @@ EmberBlog.Router.map(function() {
   this.resource('BlogPosts', { path: '/posts' }, function() {
     this.route('new');
   });
-  this.resource('BlogPost', { 
-    path: '/posts/:post_id' 
+  this.resource('BlogPost', { path: '/posts/:post_id' }, function() {
+    this.resource('comments', function() {
+      this.route('new');
+    });
+    this.route('comment', { path: 'comments/:comment_id' });
   });
 });
 
@@ -79,11 +89,48 @@ EmberBlog.IndexController = Ember.Controller.extend({
   }
 });
 
+// New Comment
+EmberBlog.CommentsNewRoute = Ember.Route.extend({
+  // http://emberjs.com/guides/routing/setting-up-a-controller/    
+  setupController: function(controller, model) {
+    controller.set('body', null);
+  }  
+});
+
+// Comments New Controller
+EmberBlog.CommentsNewController = Ember.ObjectController.extend({
+  // http://emberjs.com/guides/controllers/dependencies-between-controllers/
+  needs: 'BlogPost',
+  body: null,
+ 
+  actions: {
+    save: function() {
+      var post = this.get('controllers.BlogPost.content');
+      var comment = this.store.createRecord('BlogPostComment', { post: post, body: this.get('body') });
+      comment.save().then(function(comment) {
+        post.get('comments').pushObject(comment);
+      });
+      this.get('target').transitionTo('BlogPost.index');
+    }
+  }
+});
+
 // Add some fixtures
 EmberBlog.BlogPost.FIXTURES = [
-  { id: '1', title: 'RWX Rocks!', body: "We're learning Ember" },
-  { id: '2', title: 'HTML5 is here', body: '... to stay' },
-  { id: '3', title: 'Groovy or Ruby?', body: 'Which one to choose?' },
-  { id: '4', title: 'Is Maven making you angry?', body: 'Try Gradle!'}
+  { id: '1', title: 'RWX Rocks!', body: "We're learning Ember", comments: ['1', '5'] },
+  { id: '2', title: 'HTML5 is here', body: '... to stay', comments: ['2', '6'] },
+  { id: '3', title: 'Groovy or Ruby?', body: 'Which one to choose?', comments: ['3', '7'] },
+  { id: '4', title: 'Is Maven making you angry?', body: 'Try Gradle!', comments: ['4', '8'] }
+];
+
+EmberBlog.BlogPostComment.FIXTURES = [
+  { id:"1", body: "First Comment" }, 
+  { id:"2", body: "Second Comment" }, 
+  { id:"3", body: "Third Comment" }, 
+  { id:"4", body: "Fourth Comment" }, 
+  { id:"5", body: "Fifth Comment" }, 
+  { id:"6", body: "Sixth Comment" }, 
+  { id:"7", body: "Seventh Comment" }, 
+  { id:"8", body: "Eighth Comment" }
 ];
 
