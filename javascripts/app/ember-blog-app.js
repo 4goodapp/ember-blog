e0e9224154993c871d9280797f8fd08e36b35122
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-12-04 20:56:22 -0500

    Configure store to use Fixture Adapter/Create BlogPosts fixtures

--- javascripts/app/ember-blog-app.js
+++ javascripts/app/ember-blog-app.js
@@ -13,9 +13,20 @@ EmberBlog = Ember.Application.create({
   }
 });
 
+// Configure the Store to use the Fixture Adapter
+EmberBlog.ApplicationAdapter = DS.FixtureAdapter.extend();
+
 // A simple client-side model for a blog post
 EmberBlog.BlogPost = DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string')
 });
+
+// Add some fixtures
+EmberBlog.BlogPost.FIXTURES = [
+  { id: '1', title: 'RWX Rocks!', body: "We're learning Ember" },
+  { id: '2', title: 'HTML5 is here', body: '... to stay' },
+  { id: '3', title: 'Groovy or Ruby?', body: 'Which one to choose?' },
+  { id: '4', title: 'Is Maven making you angry?', body: 'Try Gradle!'}
+];
 
